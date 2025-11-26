b85c166443b0e43ad662c539b1d61b3cb6d993f5
Amruth <ammumandappa@gmail.com>
2025-11-26 18:41:03 +0530

    created entire increment and decrementusing redux toolkit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 
 import './App.css'
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment } from './feature/counter/counterSlice';
 
 function App() {
+
+  const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch();
+
+  function handleIncrement() {
+    dispatch(increment())
+  }
+
+  function handleDecrement() {
+    dispatch(decrement())
+  }
 
   return (
  <>
  <div >
-  HEllo
+  count: {count}
+  <button onClick={handleIncrement}>Increment</button>
+
+  <button onClick={handleDecrement}>Decrement</button>
  </div>
  </>
   )
